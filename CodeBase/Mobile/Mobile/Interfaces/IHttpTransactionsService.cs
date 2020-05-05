ea64eedb12e750cb3700b5033a9eb5d97dfc06a3
Sven Büchner <sven.buechner.privat@googlemail.com>
2020-05-05 11:24:11 +0200

    Implemented the HttpTransactionService

--- CodeBase/Mobile/Mobile/Interfaces/IHttpTransactionsService.cs
+++ CodeBase/Mobile/Mobile/Interfaces/IHttpTransactionsService.cs
@@ -2,13 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Mobile.Services
 {
     public interface IHttpTransactionsService
     {
-        Transaction GetTransaction(int transactionID);
-        List<Transaction> GetTransactionsOfBasket(int basketID);
-        List<Transaction> GetTransactionsOfUser(string userName);
+        Task<Transaction> GetTransaction(int transactionID);
+        Task<List<Transaction>> GetTransactionsOfBasket(int basketID);
+        Task<List<Transaction>> GetTransactionsOfUser(string userName);
     }
 }
---
+++ CodeBase/Mobile/Mobile/Services/HttpTransactionsService.cs
@@ -0,0 +1,79 @@
+﻿using Commons.Domain;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile.Services
+{
+    public class HttpTransactionsService : IHttpTransactionsService
+    {
+        public async Task<Transaction> GetTransaction(int transactionID)
+        {
+            //Init values
+            Transaction returnValue = null;
+
+            //Make call
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync("https://localhost:44320/api/Transactions/" + transactionID);
+
+            //Process response code
+            switch (response.StatusCode)
+            {
+                case HttpStatusCode.OK:
+                    returnValue = await response.Content.ReadAsAsync<Transaction>();
+                    break;
+                default:
+                    throw new Exception();
+
+            }
+            return returnValue;
+        }
+
+        public async Task<List<Transaction>> GetTransactionsOfBasket(int basketID)
+        {
+            //Init values
+            List<Transaction> returnValue = null;
+
+            //Make call
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync("https://localhost:44320/api/Transactions/Baskets" + basketID);
+
+            //Process response code
+            switch (response.StatusCode)
+            {
+                case HttpStatusCode.OK:
+                    returnValue = await response.Content.ReadAsAsync<List<Transaction>>();
+                    break;
+                default:
+                    throw new Exception();
+
+            }
+            return returnValue;
+        }
+
+        public async Task<List<Transaction>> GetTransactionsOfUser(string userName)
+        {
+            //Init values
+            List<Transaction> returnValue = null;
+
+            //Make call
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync("https://localhost:44320/api/Transactions/USers" + userName);
+
+            //Process response code
+            switch (response.StatusCode)
+            {
+                case HttpStatusCode.OK:
+                    returnValue = await response.Content.ReadAsAsync<List<Transaction>>();
+                    break;
+                default:
+                    throw new Exception();
+
+            }
+            return returnValue;
+        }
+    }
+}
