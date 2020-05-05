952d503c0bac38408f4356f7609cbf93a378692b
Sven Büchner <sven.buechner.privat@googlemail.com>
2020-05-05 04:29:04 +0200

    Added static method GetMockTransactions in order to mock service effectivly

--- CodeBase/Backend/Mocks/NavTransactionsServiceMock.cs
+++ CodeBase/Backend/Mocks/NavTransactionsServiceMock.cs
@@ -1,5 +1,6 @@
 ﻿using Backend.Interfaces;
 using Commons.Domain;
+using Commons.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +23,18 @@ namespace Backend.Mocks
         public List<Transaction> GetTransactionsOfUser(string userName)
         {
             throw new NotImplementedException();
+        }
+
+        public static List<Transaction> GetMockTransactions()
+        {
+            List<Transaction> transactions = new List<Transaction>();
+
+            transactions.Add(new Transaction(1, new DateTime(2020, 1, 15), PaymentType.Card, 700));
+            transactions.Add(new Transaction(2, new DateTime(2019, 4, 8), PaymentType.Check, 70));
+            transactions.Add(new Transaction(3, new DateTime(2020, 11, 4), PaymentType.Card, 7200));
+            transactions.Add(new Transaction(4, new DateTime(2028, 3, 7), PaymentType.Card, 800));
+
+            return transactions;
         }
     }
 }
