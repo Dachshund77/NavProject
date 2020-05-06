9c2f8350bcc56f59912334ddeaa850189d4788f6
Michal Provazník <rockeater0@gmail.com>
2020-05-06 14:58:56 +0200

    Added web references, removed all previous atempts at stuff

--- CodeBase/Backend/Services/NavAuthService.cs
+++ CodeBase/Backend/Services/NavAuthService.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Mobile.Droid.ExportUsersWebService;
+using System.Web;
 
 namespace Backend.Services
 {
